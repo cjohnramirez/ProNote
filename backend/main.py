6e7f8d13d35853650252c15f95d09517bf90bfe8
cjohnramirez <jcramirez340@gmail.com>
2025-06-16 22:53:08 +0800

    Still learning FastAPI

--- backend/main.py
+++ backend/main.py
@@ -1,57 +1,93 @@
-from fastapi import FastAPI, Query
-from enum import Enum
-from pydantic import BaseModel
 from typing import Annotated
-
-
-class ModelName(str, Enum):
-    alexnet = "alexnet"
-    resnet = "resnet"
-    lenet = "lenet"
-
-
-class Item(BaseModel):
-    name: str
-    description: str | None = None
-    price: float
-    tax: float | None = None
-
-
-fake_items_db = [{"item_name": "Foo"}, {"item_name": "Bar"}, {"item_name": "Baz"}]
+from fastapi import Body, FastAPI, status, HTTPException, Request
+from fastapi.responses import JSONResponse
+from pydantic import BaseModel, Field, EmailStr
 
 app = FastAPI()
 
 
-@app.get("/models/{model_name}")
-async def get_model(model_name: ModelName):
-    if model_name is ModelName.alexnet:
-        return {"model_name": model_name, "message": "Deep Learning FTW!"}
-
-    if model_name.value == "lenet":
-        return {"model_name": model_name, "message": "LeCNN all the images"}
-
-    return {"model_name": model_name, "message": "Have some residuals"}
+class Image(BaseModel):
+    url: str
+    name: str
 
 
-from typing import List, Optional
+class Item(BaseModel):
+    name: str
+    description: str | None = Field(
+        default=None, title="The desc of the item", max_length=255
+    )
+    price: float = Field(
+        gt=0, description="The price must be greater than zero", examples=[35.4]
+    )
+    tax: float | None = Field(default=None, examples=[3.2])
+    tags: set[str] = set()
+    image: Image | None = None
 
 
-@app.get("/items/{item_id}")
-async def read_amazing_item(
-    item_id: str, q: Annotated[List[str], Query()] = None, short: bool = False
+@app.put("/items/{item_id}")
+async def update_item(
+    item_id: int,
+    item: Annotated[
+        Item,
+        Body(
+            example={
+                "hehehe": {
+                    "item_id": "1",
+                    "item": {
+                        "name": "Fake Item",
+                        "description": "This is a fake item for testing.",
+                        "price": 99.99,
+                        "tax": 9.99,
+                        "tags": ["fake", "test"],
+                        "image": {
+                            "url": "https://example.com/fake-image.png",
+                            "name": "Fake Image",
+                        },
+                    },
+                }
+            }
+        ),
+    ],
 ):
-    item = {"item_id": item_id, "q": q}
-    if q:
-        item.update({"q": [s + "-id-" + item_id for s in q]})
-    if not short:
-        item.update({"description": "Wow so amazing really huhuhu"})
-    return item
+    results = {"item_id": item_id, "item": item}
+    return results
 
 
-@app.post("/items/new/")
-async def create_item(item: Item):
-    item_dict = item.dict()
-    if item.tax is not None:
-        price_with_tax = item.price + item.tax
-        item_dict.update({"price_with_tax": price_with_tax})
-    return item_dict
+@app.get("/items/", response_model=list[Item])
+async def read_items() -> any:
+    return [{"name": "Portal Gun", "price": 42.0}]
+
+
+class UserBase(BaseModel):
+    username: str
+    email: EmailStr
+    full_name: str | None = None
+
+
+class UserIn(UserBase):
+    password: str
+
+
+class UserOut(UserBase):
+    pass
+
+class UnicornException(Exception):
+    def __init__(self, name: str):
+        self.name = name
+
+@app.exception_handler(UnicornException)
+async def unicorn_exception_handler(request: Request, exc: UnicornException): 
+    return JSONResponse (
+        status_code=418,
+        content={"message": f"Oops! {exc.name} did something hehehehe."}
+    )
+
+@app.post(
+    "/user/",
+    response_model=UserOut,
+    status_code=status.HTTP_201_CREATED,
+)
+async def create_user(user: UserIn) -> any:
+    if user.username in user:
+        raise HTTPException(status_code=status.HTTP_226_IM_USED, headers={"Error" : "User already exists"})
+    return UserOut
